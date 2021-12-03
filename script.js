8d3d54f9c44390336b8887a06d11e15575dde9bd
Sandeep Bhatt <sandeepbhatt@Sandeeps-MacBook-Pro.local>
2021-12-03 14:20:51 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// $(".category").owlCarousel({
+//   loop: true,
+
+//   autoplay: true,
+//   autoplayTimeout: 5000,
+//   autoplayHoverPause: true,
+//   nav: true,
+//   navText: [
+//     "<i class='fas fa-chevron-left'></i>",
+//     "<i class='fas fa-chevron-right'></i>",
+//   ],
+//   responsive: {
+//     0: {
+//       items: 1,
+//     },
+//     350: {
+//       items: 2,
+//     },
+//     650: {
+//       items: 3,
+//     },
+//     1000: {
+//       items: 4,
+//     },
+//   },
+// });
